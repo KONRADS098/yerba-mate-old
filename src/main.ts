c12324a6da5db435e06528453a1c599b185f7183
Konrad Sokolowski <konrad-s@outlook.com>
2024-09-14 16:57:01 +0200

    add route versioning

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
 
   // TODO: read these from environment variables
   const config = new DocumentBuilder()
